6fd69e4ef0ba931a9b888efbfd6d8f90162fa413
Avni Girish <avni123.girish@gmail.com>
2022-03-15 22:23:50 -0400

    Runner and interface for printing quotient functionality created (p2)

--- quotientRunner.java
+++ quotientRunner.java
@@ -1,8 +1,15 @@
+interface Print_Quotient{
+	public void printQuotient(float num1, float num2);
+}
 public class quotientRunner{
-	public static void main(String[]args){
-		printQuotient(40f, 3f);
+	public quotientRunner(){
+		Print_Quotient quotient = (n1, n2) -> System.out.printf("%.3f%n", n1/n2);
+		quotient.printQuotient(40f, 3f);
 	}
-	public static void printQuotient (float num1, float num2){
-		System.out.printf("%.3f%n", num1/num2);
+	//public void printQuotient (float num1, float num2){
+		//System.out.printf("%.3f%n", num1/num2);
+	//}
+	public static void main (String[]args){
+		new quotientRunner();
 	}
 }
