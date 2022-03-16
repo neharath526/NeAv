471eb51dfe88e02816f52c0c9600c88562202026
Neha <neharvp@gmail.com>
2022-03-16 18:50:54 -0400

    Combined all work into one single file

--- main.java
+++ main.java
@@ -1,5 +1,18 @@
 public class main{
-	//this is the main file
-	//hello
+	interface Perfect_Square 
+    { 
+        boolean boolPerfSquare(int n); 
+    } 
+    interface Print_Quotient{ 
+	    void printQuotient(float num1, float num2); 
+    } 
+    public static void main(String args[]) {
+      Perfect_Square ps = (n1) ->  (n1 > 0 && Math.sqrt((double)n1) % 1 == 0); 
+      System.out.println(ps.boolPerfSquare(25)); 
+      System.out.println(ps.boolPerfSquare(98)); 
+      
+      Print_Quotient quotient = (n1, n2) -> System.out.printf("Quotient: %.3f%n", n1/n2); 
+	  quotient.printQuotient(40f, 3f); 
+    }
 
 }
