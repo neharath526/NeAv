708e704faadaddba4edd27d3d81e41a9ef2db385
Neha <neharvp@gmail.com>
2022-03-16 10:18:24 -0400

    Runner and interface for perfect square functionary created

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+interface Perfect_Square
+{
+    //public boolean printPS(int n1);
+}
+namespace PSquare
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
